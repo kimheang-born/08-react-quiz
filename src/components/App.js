c25faf008fc5215fb5053ce449ae3dc7a22902ec
Kimheang <kimheang135@gmail.com>
2023-11-19 17:21:53 +0700

    chore: handling new answers

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,8 @@ const initialState = {
   // 'loading', 'error', 'ready', 'active', 'finished'
   status: 'loading',
   index: 0,
+  answer: null,
+  points: 0,
 };
 function reducer(state, action) {
   switch (action.type) {
@@ -31,6 +33,17 @@ function reducer(state, action) {
         ...state,
         status: 'active',
       };
+    case 'newAnswer':
+      const question = state.questions.at(state.index);
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
 
     default:
       throw new Error('Unknown action');
@@ -38,7 +51,7 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -62,7 +75,13 @@ export default function App() {
         {status === 'ready' && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === 'active' && <Question question={questions.at(index)} />}
+        {status === 'active' && (
+          <Question
+            question={questions.at(index)}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
       </Main>
     </div>
   );
